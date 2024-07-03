a37013e3dc7c181958832e8802e8330579f77950
EgorZenzin <zenzin48@mail.ru>
2024-07-03 13:25:00 +0600

    Add ScrollTabBar

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -29,15 +29,92 @@ class _CoffeeShopPageState extends State<CoffeeShopPage> with SingleTickerProvid
   final ScrollController _scrollController = ScrollController();
   final ScrollController _tabScrollController = ScrollController();
   final Map<String, GlobalKey> _categoryKeys = {};
+  late TabController _tabController;
   int _selectedCategoryIndex = 0;
+  bool _isTabClick = false;
 
   @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: categories.length, vsync: this);
+    for (var category in categories) {
+      _categoryKeys[category] = GlobalKey();
+    }
+    _scrollController.addListener(_onScroll);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_onScroll);
+    _scrollController.dispose();
+    _tabScrollController.dispose();
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  void _onScroll() {
+    if (_isTabClick) return;
+
+    for (int i = 0; i < categories.length; i++) {
+      final category = categories[i];
+      final key = _categoryKeys[category];
+      if (key != null) {
+        final context = key.currentContext;
+        if (context != null) {
+          final box = context.findRenderObject() as RenderBox;
+          final position = box.localToGlobal(Offset.zero).dy;
+          if (position >= 0 && position <= MediaQuery.of(context).size.height / 2) {
+            setState(() {
+              _selectedCategoryIndex = i;
+              _tabController.index = i;
+            });
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  void _scrollToCategory(int index) {
+    final category = categories[index];
+    final key = _categoryKeys[category];
+    if (key != null) {
+      setState(() {
+        _isTabClick = true;
+        _selectedCategoryIndex = index;
+        _tabController.index = index;
+      });
+      Scrollable.ensureVisible(
+        key.currentContext!,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      ).then((_) {
+        setState(() {
+          _isTabClick = false;
+        });
+      });
+      _scrollTabBarToIndex(index);
+    }
+  }
+
+  void _scrollTabBarToIndex(int index) {
+    const tabWidth = 100.0;
+    final offset = index * tabWidth;
+    _tabScrollController.animateTo(
+      offset,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeInOut,
+    );
+  }
+
+
+   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
           Padding(
-            padding: const EdgeInsets.only(left: 32.0, top: 16.0),
+            padding: const EdgeInsets.only(left: 32.0, top: 32.0),
             child: PreferredSize(
               preferredSize: const Size.fromHeight(48.0),
               child: SingleChildScrollView(
@@ -52,6 +129,9 @@ class _CoffeeShopPageState extends State<CoffeeShopPage> with SingleTickerProvid
                         label: Text(category),
                         selected: isSelected,
                         onSelected: (selected) {
+                          if (selected) {
+                            _scrollToCategory(categories.indexOf(category));
+                          }
                         },
                         selectedColor: CoffeeAppColors.activeCategoryBackground,
                         backgroundColor: CoffeeAppColors.categoryBackground,
